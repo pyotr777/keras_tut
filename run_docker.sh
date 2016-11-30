4c2e05142a6c39808b2196d9fce8b06e98262d39
Peter Bryzgalov <peterbryz@yahoo.com>
2016-11-30 11:14:20 +0900

    Init

---
+++ run_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mntdir="/root/keras"
+echo "Start Docker container for Keras mounting current dir to $mntdir"
+docker run -ti -v $(pwd):$mntdir -p 8888:8888 --name 4keras pyotr777/keras-jupyter /bin/bash
+
